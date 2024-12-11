0bba9d9735ebda368363cb854edab98ba333913c
Jose Covian <jose@covian.es>
2024-12-11 12:04:17 +0100

    Limpieza de código

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [cards, setCards] = useState([]);
   const [flippedCards, setFlippedCards] = useState([]);
   const [matchedCouples, setMatechedCouples] = useState(0);
+  const [tries, setTries] = useState(0);
   const [activeCard, setActiveCard] = useState([]);
   const [firstCard, setFirstCard] = useState([]);
 
@@ -17,7 +18,7 @@ function App() {
     setCards(mixedCards);
   }, []);
 
-  //Muestra un mensaje al completar el juego
+  //Muestra un mensaje al completar el juego y lo resetea
   useEffect(() => {
     if (matchedCouples === (cards.length / 2) && (cards.length > 0)) {
       alert("¡FELICIDADES! Has completado el juego");
@@ -29,8 +30,6 @@ function App() {
     if (flippedCards.length < 2 && !flippedCards.includes(card)) {
       setFlippedCards((i) => [...i, card]);
       setActiveCard(card);
-      console.log("Carta pinchada: ");
-      console.log(card);
     }
   }
 
@@ -44,77 +43,58 @@ function App() {
       //Guarda copia de la primera carta volteada para más tarde
       setFirstCard(activeCard);
     }
-    
+
     if (flippedCards.length === 2) {
       //Actualiza segunda carta volteada
       setCards(newActiveCard);
+      setTries(tries + 1);
 
       if (newFlippedCards[0].tipo === newFlippedCards[1].tipo) {
-        console.log("MATCH");
-        setTimeout(() => matchCards(newFlippedCards), 1000);
+        //Marca las cartas como emparejadas
+        matchCards(newFlippedCards);
+        //Borra la primera carta guardada, ya no hace falta
         setFirstCard([]);
       } else {
-        console.log("Cartas no coinciden ");
-        console.log(newFlippedCards);
-        console.log("NO MATCH");
+        //No coinciden las cartas, se espera un segundo y se voltean
         setTimeout(() => unflipCards(newFlippedCards), 1000);
       };
     };
   }, [flippedCards]);
 
+  //Marca las cartas como emparejadas
   const matchCards = (flippedCards) => {
     const newCard = cards.map(i => flippedCards.includes(i) ? { ...i, matched: true } : i);
-    //console.log("match array: ");
-    //console.log(newCard);
     setCards(newCard);
     setFlippedCards([]);
     setMatechedCouples(matchedCouples + 1);
   };
 
+  //Función que pone las cartas boca abajo
   const unflipCards = (flippedCards) => {
-    console.log("contenido flippercards unFlip");
-    console.log(flippedCards);
-    
-    //const newCard = cards.map(i => flippedCards.includes(i) ? { ...i, flipped: false } : i);
-    //setCards(newCard);
-    //const newCard = cards.map(i => i.id === flippedCards.id ? { ...i, flipped: false } : i);
-    let newCard = cards.map(item => flippedCards.includes(item) ? { ...item, flipped: false } : item);
+    //Voltea la segunda carta seleccionada
+    let newCard = cards.map(i => flippedCards.includes(i) ? { ...i, flipped: false } : i);
     setCards(newCard);
-    console.log("contenido firstCard");
-    console.log(firstCard);
-    newCard = cards.map(item => item.id === firstCard.id ? { ...item, flipped: false } : item);
+    //Voltea la primera carta seleccionada
+    newCard = cards.map(i => i.id === firstCard.id ? { ...i, flipped: false } : i);
     setCards(newCard);
-    
-    //const newCard = cards.map(item1 => flippedCards.find(item2 => item1.id === item2.id) ? item1.flipped = false: item1);
-    //const newCard = flippedCards.map(item => return{ {...item, flipped: false}});
 
-    //const newActiveCard = cards.map(i => i.id === activeCard.id ? { ...i, flipped: true } : i);
-    
-  
-    console.log("contenido newCard en unFlip")
-    console.log(newCard);
-    //setCards(newCard);
     setFlippedCards([]);
     setFirstCard([]);
-    
-    console.log("UnFlip cards: ");
-    console.log(cards);
   };
-
 
   return (
     <div className="container-fluid">
 
       { /*Cabecera*/}
-      <header className="text-center border-bottom py-2 my-2">
-        <h1>Memory Card App V0.1α</h1>
-        <h3>Parejas logradas: {matchedCouples}</h3>
+      <header className="text-center border-bottom py-2 my-2 alert alert-primary">
+        <h1>Memory Card App V0.1&beta;</h1>
+        <h3>Parejas logradas: {matchedCouples} - Intentos: {tries} </h3>
         {/*<button>REINICIAR</button>*/}
       </header>
 
       {/* Tablero */}
       <section className="container mt-1 text-center">
-        <div className="row row-cols-lg-4 row-cols-md-4 row-cols-2" >
+        <div className="row row-cols-lg-8 row-cols-md-4 row-cols-2" >
           { /*Cartas*/}
           {
             cards.map((card) => (
@@ -122,15 +102,13 @@ function App() {
             ))
           }
         </div>
-
       </section>
 
       { /*Pie de página*/}
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top alert alert-primary">
         <p className="mb-0 text-muted">&copy; 2024 JCovian</p>
         <p className="mb-0 text-muted">Módulo Diseño de Interfaces - DAM Distancia CIFP Avilés</p>
       </footer>
-
     </div>
   );
 };
